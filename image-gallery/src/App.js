134c6b0eac6a92f66bc03a0daadd84b3201e3692
areebaqamar021 <areebaqamar021@gmail.com>
2021-05-04 13:40:11 +0500

    new work

--- image-gallery/src/App.js
+++ image-gallery/src/App.js
@@ -1,10 +1,30 @@
-import React from 'react';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import { Heading } from './components/Heading';
+import { Loader } from './components/Loader';
+import { UnsplashImage } from './components/UnsplashImage';
+
+import axios from 'axios';
 
 function App() {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    const apiRoot = "https://api.unsplash.com";
+    const accessKey = process.env.React_APP_ACCESSKEY;
+
+    axios
+      .get(`${apiRoot}/photos/random?client_id=${accessKey}&count=10`)
+      .then(res => setImages([...images, ...res.data]))
+      
+  }, [])
+
   return (
     <div className="App">
-      Image Gallery
+      <Heading />
+      <Loader />
+      {images.map(image => (
+        <UnsplashImage url={image.urls.thumb} key={image.id} />
+      ))}
     </div>
   );
 }
