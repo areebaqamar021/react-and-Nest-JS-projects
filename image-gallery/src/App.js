69ac89a72b771b3bd604fdf15099fd15e23c0bc9
areebaqamar021 <areebaqamar021@gmail.com>
2021-06-08 12:01:50 +0500

    image gallery

--- image-gallery/src/App.js
+++ image-gallery/src/App.js
@@ -9,7 +9,25 @@ import styled from 'styled-components';
 import { createGlobalStyle } from 'styled-components';
 
 // Style
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  body {
+    font-family: sans-serif;
+  }
+`;
 
+const WrapperImages = styled.section`
+  max-width: 70rem;
+  margin: 4rem auto;
+  display: grid;
+  grid-gap: 1em;
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  grid-auto-rows: 300px;
+`;
 
 function App() {
   const [images, setImage] = useState([]);
