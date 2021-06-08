661cbcd5ac85508f5363ebeef3df3ade13361430
areebaqamar021 <areebaqamar021@gmail.com>
2021-06-08 11:27:53 +0500

    covid app

---
+++ covidapp-react/src/App.js
@@ -0,0 +1,14 @@
+  
+import logo from './logo.svg';
+import './components/Appi.css';
+import Api from './components/Api'
+import { Container } from '@material-ui/core';
+function App() {
+  return (
+    <Container>
+     <Api /> 
+     </Container>
+       );
+}
+
+export default App;
