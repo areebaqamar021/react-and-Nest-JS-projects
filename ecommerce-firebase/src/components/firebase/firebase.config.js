5bbc8b37cce743a8e145a3031e09565e33310ba0
areebaqamar021 <areebaqamar021@gmail.com>
2021-06-08 10:41:09 +0500

    facebook auth undone

---
+++ ecommerce-firebase/src/components/firebase/firebase.config.js
@@ -0,0 +1,13 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyByeVAY92qkXS77Um-bKPhzU-aJcUVDoGU",
+  authDomain: "food-firebase-bf7c7.firebaseapp.com",
+  projectId: "food-firebase-bf7c7",
+  storageBucket: "food-firebase-bf7c7.appspot.com",
+  messagingSenderId: "366177116865",
+  appId: "1:366177116865:web:b33c297b977ee7f70eaa40",
+  measurementId: "G-G1RSPD8787"
+  };
+
+
+export default firebaseConfig;
