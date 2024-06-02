f94e37446444e3cd3785e0fea879f944e5d068a0
areebaqamar021 <areebaqamar021@gmail.com>
2024-06-02 14:57:11 +0500

    Implemented components:   - Button.js: Represents individual calculator buttons   - Display.js: Displays current input and results   - ButtonPanel.js: Contains all calculator buttons

--- scientific-calculator/src/Components/Button.js
+++ scientific-calculator/src/Components/Button.js
@@ -1,11 +1,11 @@
-import React from 'react';
+// import React from 'react';
 
-const Button = ({ label, onClick }) => {
-  return (
-    <button className="button" onClick={() => onClick(label)}>
-      {label}
-    </button>
-  );
-};
+// const Button = ({ label, onClick }) => {
+//   return (
+//     <button className="button" onClick={() => onClick(label)}>
+//       {label}
+//     </button>
+//   );
+// };
 
-export default Button
+// export default Button
