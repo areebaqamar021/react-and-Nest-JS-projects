0ccf975615622ab882d9175412c93282c1a510fc
areebaqamar021 <areebaqamar021@gmail.com>
2024-06-02 14:57:49 +0500

    Implemented components:   - Button.js: Represents individual calculator buttons   - Display.js: Displays current input and results   - ButtonPanel.js: Contains all calculator buttons

--- scientific-calculator/src/Components/ButtonPanel.js
+++ scientific-calculator/src/Components/ButtonPanel.js
@@ -1,23 +1,23 @@
-// import React from 'react'
-// import Button from './Button';
+import React from 'react'
+import Button from './Button';
 
-// const ButtonPanel = ({onButtonClick}) => {
-//     const buttons = [
-//         '7', '8', '9', '/', 'sin',
-//         '4', '5', '6', '*', 'cos',
-//         '1', '2', '3', '-', 'tan',
-//         '0', '.', '=', '+', 'log',
-//         '(', ')', '^', 'sqrt', 'clear',
-//         'pi', 'e', 'exp', 'abs', 'fac',
-//         'ln', 'deg', 'rad', '%', 'Ans'
-//       ];
-//   return (
-//     <div className="button-panel">
-//       {buttons.map((button, index) => (
-//         <Button key={index} label={button} onClick={onButtonClick} />
-//       ))}
-//     </div>
-//   )
-// }
+const ButtonPanel = ({onButtonClick}) => {
+    const buttons = [
+        '7', '8', '9', '/', 'sin',
+        '4', '5', '6', '*', 'cos',
+        '1', '2', '3', '-', 'tan',
+        '0', '.', '=', '+', 'log',
+        '(', ')', '^', 'sqrt', 'clear',
+        'pi', 'e', 'exp', 'abs', 'fac',
+        'ln', 'deg', 'rad', '%', 'Ans'
+      ];
+  return (
+    <div className="button-panel">
+      {buttons.map((button, index) => (
+        <Button key={index} label={button} onClick={onButtonClick} />
+      ))}
+    </div>
+  )
+}
 
-// export default ButtonPanel;
+export default ButtonPanel;
