5c16cf9dca013019e2a1f841ba5bb37a12ab8a9b
areebaqamar021 <areebaqamar021@gmail.com>
2024-06-18 12:06:43 +0500

    feat: Add login and sign-up functionality with email and password validation
    - Created LoginForm component for user login
    - Created SignUpForm component for new user registration

---
+++ firebase-auth/src/Firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCMsP4Dp3hUHRweI8grtNoeMOKpn5wyg6c",
+    authDomain: "login-auth-d69c3.firebaseapp.com",
+    projectId: "login-auth-d69c3",
+    storageBucket: "login-auth-d69c3.appspot.com",
+    messagingSenderId: "584090562976",
+    appId: "1:584090562976:web:ca13bc61273a3d204b74c5",
+    measurementId: "G-80LW3M0WF6"
+  };
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth };
