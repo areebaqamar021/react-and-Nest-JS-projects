273c007de5a5484cf45a3a1b24f4583ae3add4dd
areebaqamar021 <areebaqamar021@gmail.com>
2024-08-21 19:48:32 +0500

    feat: Add WithdrawalRate component to display candidate withdrawal rate in recruitment metrics

---
+++ recruitment-metrics/src/components/Charts/WithdrawalRate.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { GaugeChart } from 'react-gauge-chart';
+
+const WithdrawalRate = ({ data }) => (
+    <div className="withdrawal-rate">
+        <h3>Candidate Withdrawal Rate</h3>
+        <GaugeChart id="gauge-chart2" 
+                    nrOfLevels={30} 
+                    percent={data / 100} 
+                    colors={['#00C49F', '#FFBB28']} />
+    </div>
+);
+
+export default WithdrawalRate;
