7fc2a74119b7f66ca288ca59189fcbc0a0d4cfb4
areebaqamar021 <areebaqamar021@gmail.com>
2024-08-21 19:48:00 +0500

    feat: Add OfferAcceptance component to display offer acceptance in recruitment metrics

---
+++ recruitment-metrics/src/components/Charts/OfferAcceptance.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { GaugeChart } from 'react-gauge-chart';
+
+const OfferAcceptance = ({ data }) => (
+    <div className="offer-acceptance">
+        <h3>Offer Acceptance</h3>
+        <GaugeChart id="gauge-chart1" 
+                    nrOfLevels={30} 
+                    percent={data / 100} 
+                    colors={['#FF5F6D', '#FFC371']} />
+    </div>
+);
+
+export default OfferAcceptance;
