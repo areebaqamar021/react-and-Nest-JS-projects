6b9ed662b5897122a5a622b6c9b05f4f693b538e
areebaqamar021 <areebaqamar021@gmail.com>
2024-08-21 19:47:30 +0500

    feat: Add GenderRatioChart component to display gender ratio in recruitment metrics

---
+++ recruitment-metrics/src/components/Charts/GenderRatioChart.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { PieChart, Pie, Cell, Tooltip, ResponsiveContainer } from 'recharts';
+
+const COLORS = ['#FFBB28', '#FF8042'];
+
+const GenderRatioChart = ({ data }) => (
+    <ResponsiveContainer width="100%" height={300}>
+        <PieChart>
+            <Pie data={data} dataKey="value" nameKey="gender" cx="50%" cy="50%" outerRadius={100} fill="#8884d8">
+                {data.map((entry, index) => (
+                    <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                ))}
+            </Pie>
+            <Tooltip />
+        </PieChart>
+    </ResponsiveContainer>
+);
+
+export default GenderRatioChart;
