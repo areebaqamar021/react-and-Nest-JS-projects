e20de229e39e0eaca7621928677480e04d3e8d17
areebaqamar021 <areebaqamar021@gmail.com>
2024-10-12 23:45:38 +0500

    "Added TaskList component and imported it into App.js, replacing static text with a form for adding tasks."

--- task-app/src/components/TaskList.js
+++ task-app/src/components/TaskList.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import { useState } from "react"
 
 const TaskList = () => {
+    const [tasks, setTasks] = useState([]);
+    const [input, setinput] =useState('');
+
+    const addTask = () => {
+        if(input.trim() === ''){
+            return;
+        }
+       setTasks([...tasks, input])
+       setTasks(''); 
+    }
   return (
     <div>
-      
+      <form>
+        <input type="text" name="task" />
+        <button>Add Task</button>
+      </form>
     </div>
   )
 }
