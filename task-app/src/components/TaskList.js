8dcb8210db04e4d839300910310c3bcbbe800f43
areebaqamar021 <areebaqamar021@gmail.com>
2024-10-14 13:12:21 +0500

    Refactor TaskList component to update input state variable name and improve task rendering

--- task-app/src/components/TaskList.js
+++ task-app/src/components/TaskList.js
@@ -46,29 +46,41 @@ const TaskList = () => {
           placeholder="Enter a Task"
         />
         {editIndex !== null ? (
-          <button 
-          className="ml-2 bg-yellow-500 hover:bg-yellow-600 text-white py-2 px-4 rounded-lg"  
-          onClick={updateTask}>Update Task</button>
+          <button
+            className="ml-2 bg-yellow-500 hover:bg-yellow-600 text-white py-2 px-4 rounded-lg"
+            onClick={updateTask}
+          >
+            Update Task
+          </button>
         ) : (
-          <button 
-          className="ml-2 bg-yellow-500 hover:bg-yellow-600 text-white py-2 px-4 rounded-lg"
-          onClick={addTask}>Add</button>
+          <button
+            className="ml-2 bg-yellow-500 hover:bg-yellow-600 text-white py-2 px-4 rounded-lg"
+            onClick={addTask}
+          >
+            Add
+          </button>
         )}
       </div>
       <ul>
         {tasks.map((t, index) => (
-          <li 
-          key={index} 
-          className="bg-gray-100 p-3 mb-2 rounded-lg flex justify-between items-center"
+          <li
+            key={index}
+            className="bg-gray-100 p-3 mb-2 rounded-lg flex justify-between items-center"
           >
             <span className="text-lg">{t}</span>
             <div className="flex-shrink-0 flex space-x-2">
-            <button className="mr-2 bg-green-500 hover:bg-green-600 text-white py-1 px-3 rounded-lg" onClick={() => startEditTasks(index)}>
-              Edit
-            </button>
-            <button className="bg-red-500 hover:bg-red-600 text-white py-1 px-3 rounded-lg" onClick={() => deleteTask(index)}>
-              Delete
-            </button>
+              <button
+                className="mr-2 bg-green-500 hover:bg-green-600 text-white py-1 px-3 rounded-lg"
+                onClick={() => startEditTasks(index)}
+              >
+                Edit
+              </button>
+              <button
+                className="bg-red-500 hover:bg-red-600 text-white py-1 px-3 rounded-lg"
+                onClick={() => deleteTask(index)}
+              >
+                Delete
+              </button>
             </div>
           </li>
         ))}
