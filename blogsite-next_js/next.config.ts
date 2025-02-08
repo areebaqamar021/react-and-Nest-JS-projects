14d03cc4d6105f55364988a10b3cd38e78a8284a
areebaqamar021 <areebaqamar021@gmail.com>
2025-02-08 22:24:59 +0500

    Update Next.js and Tailwind CSS configuration for image domains and content paths

--- blogsite-next_js/next.config.ts
+++ blogsite-next_js/next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['cdn.dummyjson.com'],
+  },
+  env: {
+    BASE_URL: process.env.BASE_URL
+  }
 };
 
 export default nextConfig;
