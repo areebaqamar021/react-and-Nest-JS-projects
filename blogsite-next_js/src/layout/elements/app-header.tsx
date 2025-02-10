06d4440b713630e251d3ece98d8a902f53078064
areebaqamar021 <areebaqamar021@gmail.com>
2025-02-10 18:08:56 +0500

    Add AppBanner component and enhance AppHeader with navigation and search functionality

--- blogsite-next_js/src/layout/elements/app-header.tsx
+++ blogsite-next_js/src/layout/elements/app-header.tsx
@@ -1,10 +1,58 @@
-import React from 'react'
+import { Button, Layout, Space } from 'antd';
+import React from 'react';
+import type { MenuProps } from 'antd';
+import { Menu } from 'antd';
+import Image from 'next/image';
+import Link from 'next/link';
 
+type MenuItem = Required<MenuProps>['items'][number];
+
+const { Header } = Layout
+const items: MenuItem[] = [
+  {
+    label: <Link href='/'>Home</Link>,
+    key: 'home',
+  },
+  {
+    label: <Link href='/posts'>Posts</Link>,
+    key: 'posts',
+  },
+  {
+    label: <Link href='/about'>About</Link>,
+    key: 'about',
+  },
+  {
+    label: <Link href='/contact'>Contacts</Link>,
+    key: 'contacts',
+  },
+];
 function AppHeader() {
   return (
-    <div>
-      AppHeader
-    </div>
+    <Header className='bg-white flex items-center h-20'>
+      <Image src={"/images/logo.png"} alt='' width={100} height={100} />
+      <Menu className='border-b-0 flex-1 flex justify-center items-center text-base font-semibold' mode="horizontal" items={items} />
+      <Space size="large">
+        <Button
+          shape="circle"
+          type="text"
+          className="text-xl"
+        >
+          <Image
+            src="/images/search.png"
+            alt="search"
+            width={20}
+            height={10}
+          />
+        </Button>
+        <Button
+          shape="circle"
+          type="text"
+          className="text-xl font-semibold"
+        >
+          Login
+        </Button>
+      </Space>
+    </Header>
   )
 }
 
---
+++ blogsite-next_js/src/screens/home/elements/app-banners.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AppBanner() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AppBanner
