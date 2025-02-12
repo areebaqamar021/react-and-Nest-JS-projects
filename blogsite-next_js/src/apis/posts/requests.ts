46e48eebcc56d10374d51903261da12cb2851227
areebaqamar021 <areebaqamar021@gmail.com>
2025-02-12 20:55:11 +0500

    Add search functionality to PostsScreen and update API request for posts

--- blogsite-next_js/src/apis/posts/requests.ts
+++ blogsite-next_js/src/apis/posts/requests.ts
@@ -2,9 +2,9 @@ import { api } from "@src/lib";
 import { useQuery } from "@tanstack/react-query";
 import { GetPostsInput, GetPostsResponse } from "./dto";
 
-export const useGetPosts = ({ limit }: GetPostsInput) => useQuery({
-    queryKey: ["posts", limit],
-    queryFn: async () => (await api.get<GetPostsResponse>("/posts", {
-        params: { limit }
+export const useGetPosts = ({ limit, search }: GetPostsInput) => useQuery({
+    queryKey: ["posts", limit, search],
+    queryFn: async () => (await api.get<GetPostsResponse>(`/posts${search ? "/search" : ""}`, {
+        params: { limit, q : search }
     })).data,
 });
