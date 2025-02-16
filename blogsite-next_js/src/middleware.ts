ac105fa2e4380c1d8fc98b8ffd018cdab5561290
areebaqamar021 <areebaqamar021@gmail.com>
2025-02-16 19:42:36 +0500

    Refactor authentication setup by removing unused code and simplifying middleware export

--- blogsite-next_js/src/middleware.ts
+++ blogsite-next_js/src/middleware.ts
@@ -1,13 +1,1 @@
-import { auth } from "@src/lib"
-import { NextResponse } from "next/server"
-
-export default auth((req) => {
-    if (!!req.auth?.user?.email && req.nextUrl.pathname === "/login") {
-        return NextResponse.redirect(new URL("/", req.nextUrl))
-    }
-    return NextResponse.next()
-})
-
-export const config = {
-    matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
-}
+export { auth as middleware } from "@/auth"
